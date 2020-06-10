24d5ebe4935143b2db8b7324914140cf87f24f07
DenisTby <denmulok@gmail.com>
2020-06-10 19:39:12 +0300

    Angular routing with child-routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,23 @@
-import { NgModule } from '@angular/core';
+import { HomeComponent } from './home/home.component';
+import { EditProfileComponent } from './edit-profile/edit-profile.component';
+import { ManageNotificationComponent } from './manage-notification/manage-notification.component';
+import { ViewActivitiesComponent } from './view-activities/view-activities.component';
+
+import { AppComponent } from './app.component';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo:'/home', pathMatch:'full'},
+  {path:'home', component:HomeComponent, children:[
+    {path: '', component:EditProfileComponent},
+    {path:'viewActivities', component:ViewActivitiesComponent},
+    {path:'notification', component:ManageNotificationComponent},
+    {path:'profileEdit', component:EditProfileComponent}
+  ]
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ViewActivitiesComponent } from './view-activities/view-activities.component';
+import { ManageNotificationComponent } from './manage-notification/manage-notification.component';
+import { EditProfileComponent } from './edit-profile/edit-profile.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ViewActivitiesComponent,
+    ManageNotificationComponent,
+    EditProfileComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
